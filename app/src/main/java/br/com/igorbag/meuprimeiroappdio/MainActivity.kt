8d2b8fb62fa0f40c44682e0a7543e62ef655d707
ingridolitk <70184804+luizsfl@users.noreply.github.com>
2023-10-07 12:42:48 -0300

    Primeira parte do programa

--- app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
+++ app/src/main/java/br/com/igorbag/meuprimeiroappdio/MainActivity.kt
@@ -1,14 +1,24 @@
 package br.com.igorbag.meuprimeiroappdio
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.com.igorbag.meuprimeiroappdio.databinding.ActivityMainBinding
+
+//O desafio sera criar uma valor dentro do string.xml
+// E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewbinding  by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(viewbinding.root)
 
-        //O desafio sera criar uma valor dentro do string.xml
-        // E trocar o texto do xml e tornar internacional (Ingles, Espanhol, etc...)
+        viewbinding.selecionarIdioma.setOnClickListener {
+            val intent = Intent(this, ConfiguraIdioma::class.java)
+            startActivity(intent)
+        }
     }
+
 }
